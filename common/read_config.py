f110355bbcb3c6f5b208d7371f6d1a5708a4af36
Eric Flumerfelt <eflumerf@fnal.gov>
2025-07-18 13:32:56 -0500

    Initial version of start_daq

--- common/read_config.py
+++ common/read_config.py
@@ -17,7 +17,7 @@ def load_config(input_file):
 @click.option('--input_file', '-f', help="Config file to read", type=click.Path(exists=True), default="~/daq-shifter-tools/daq-operations.yaml")
 @click.option('--partition', '-z', help="Partition to load", default="partition_0")
 @click.option("--environment", "-e", help="Environment to load", default=None, type=str)
-@click.argument("command_verb", type=click.Choice(["directory", "setup", "print"], case_sensitive=False))
+@click.argument("command_verb", type=str, default="print")
 def read_config(input_file, partition, environment, command_verb):
     config = load_config(input_file)
     if partition not in config['partitions']:
@@ -32,13 +32,20 @@ def read_config(input_file, partition, environment, command_verb):
     if environment != None:
         environment_config = partition_config['environments'][environment]
 
-    if command_verb == "directory":
+    if command_verb.lower() == "directory":
         print(environment_config['test_rel_path'])
-    elif command_verb == "setup":
+    elif command_verb.lower() == "setup":
         print(environment_config['setup_cmd'])
-    elif command_verb == "print":
+    elif command_verb.lower() == "print":
         pprint(config)
-        
+    elif command_verb.lower() == "active-envs":
+        output_str=""
+        for env in partition_config['active_environments']:
+            output_str += env + " "
+        print(output_str)
+    else:
+        sys.stderr.write(f"ERROR: Unrecognized command {command_verb}!")
+        sys.exit(3)
 
 if __name__ == '__main__':
    read_config()
