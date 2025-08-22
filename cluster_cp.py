432e3fecc1148e4d514ecc6c40e180aa433a29dc
Mu2e <mu2edaq@mu2edaq13.fnal.gov>
2025-08-22 15:06:23 -0500

    Added files for working across the cluster

---
+++ cluster_cp.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+import os
+import json
+import argparse
+import subprocess  
+import concurrent.futures
+
+# Copy files to a location across the full DAQ cluster
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Copy files to a location across the full DAQ cluster.")
+parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output.")
+parser.add_argument("-c","--calo", action="store_true", help="Copy files to the Calo nodes.")
+parser.add_argument("-C","--crv", action="store_true", help="Copy files to the CRV nodes.")
+parser.add_argument("-t","--trk", action="store_true", help="Copy files to the Trk nodes.")
+parser.add_argument("-d","--daq", action="store_true", help="Copy files to the DAQ nodes.")
+parser.add_argument("-a","--all", action="store_true", help="Copy files to all nodes.")
+parser.add_argument("-u", "--user", help="Username for the remote nodes.")
+parser.add_argument("files", nargs="+", help="List of files to copy.")
+parser.add_argument("destination", nargs=1,  help="Destination path on the remote nodes.")
+args = parser.parse_args()
+
+verbose = args.verbose
+
+# print(args.files)
+# print(args.destination)
+
+user = args.user
+files = args.files
+destination = args.destination
+
+# First read in the list of nodes to copy files to
+# Path to the JSON file containing the list of nodes
+nodes_file_path = "/home/mu2edaq/DAQ-Test-Releases/daq-shifter-tools/"
+nodes_file = "daq_nodes.json"
+
+user = "mu2edaq"
+
+# Read the JSON file and load the list of nodes
+with open(nodes_file_path + nodes_file, "r") as f:
+    nodes = json.load(f)
+
+# Ensure the nodes are in a list format
+#if not isinstance(nodes, list):
+#    raise ValueError("The JSON file must contain a list of node names.")
+
+hosts=[]
+if not args.calo and not args.crv and not args.trk and not args.daq:
+    # print("No hosts specified")
+    args.all = True
+
+if args.calo:
+    hosts += nodes["calo"]
+if args.crv:
+    hosts += nodes["crv"]
+if args.trk:
+    hosts += nodes["trk"]
+if args.daq:
+    hosts += nodes["daq"]
+if args.all:
+    hosts = nodes["daq"] + nodes["trk"] + nodes["crv"] + nodes["calo"]
+if verbose:
+    print(f"Using hosts list: {hosts}")
+
+def copy_file_to_host(file, host, user, destination):
+    ret=0
+    ret = os.system(f"scp {file} {user}@{host}:{destination}")
+    if ret != 0:
+        print(f"Error: copying {file} to {user}@{host}:{destination}")
+    else:
+        if verbose:
+            print(f"Completed copying {file} to {user}@{host}:{destination}")
+    return ret
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = []
+    for f in files:
+        for h in hosts:
+            # copy_file_to_host(f, h, user, destination[0])
+            futures.append(executor.submit(copy_file_to_host, f, h, user, destination[0]))
+    
+    # Wait for all futures to complete
+    concurrent.futures.wait(futures)
+    if verbose:
+        print("All file copy operations submitted.")
+
+
+    #for future in futures:
+        #print(future)
+        #if future.result() != 0:
+        #    print(f"File copy operation failed.")
+        #else:
+        #    print(f"File copy operation succeeded.")
---
+++ install_daq_tools.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+import argparse
+import yaml
+import os
+import sys
+
+# Version number padding
+version_pad = 2
+
+# Default filename to use for release information
+default_release_file = "current_release.yaml"
+
+# Version schema
+version_schema = {
+    "prefix": "d",
+    "major": 0,
+    "minor": 0,
+    "patch": 0,
+    "suffix": ""
+}
+
+def build_version_string(version_info):
+    theString = ""
+    for key in version_schema.keys():
+        if key == "prefix":
+            theString = version_info[key]
+        if key == "major":
+            theString = theString + str(version_info[key]).rjust(version_pad, '0')
+        if key == "minor":
+            theString = theString + "_" + str(version_info[key]).rjust(version_pad, '0')
+        if key == "patch":
+            theString = theString + "_" + str(version_info[key]).rjust(version_pad, '0')
+        if key == "suffix":
+            if version_info[key] != "":
+                theString = theString + "_" + str(version_info[key])
+                pass
+    return theString
+
+def parse_yaml(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = yaml.safe_load(file)
+        return data
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        sys.exit(1)
+    except yaml.YAMLError as e:
+        print(f"Error parsing YAML file: {e}")
+        sys.exit(1)
+
+def main():
+    parser = argparse.ArgumentParser(description="Parse YAML file and extract DAQ tool paths.")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.0")
+    parser.add_argument("-O",'--override', action='store_true', help="Override fatal errors and continue.")   
+    parser.add_argument("-M", '--makedirs', action='store_true', help="Create missing install directories.")
+    parser.add_argument("-f",'--yaml_file', type=str, default=default_release_file, help="Path to the YAML configuration file.")
+    args = parser.parse_args()
+
+    if args.yaml_file:
+        config = parse_yaml(args.yaml_file)
+    else:
+        config = parse_yaml(default_release_file)
+
+    base_release = config.get('base_release')
+    test_release = config.get('test_release')
+    install_paths = config.get('install_path', {})
+
+    # Require that the YAML have all the required fields    
+    if not base_release or not test_release or not install_paths:
+        print("Error: Missing required fields in the YAML file.")
+        sys.exit(1)
+
+    
+    #print("Install Paths:")
+    #for key, path in install_paths.items():
+    #    print(f"  {key}: {path}")
+
+    # Build the version strings
+    base_release_str = build_version_string(base_release)
+    test_release_str = build_version_string(test_release)
+
+    # Print the release information that is found   
+    print(f"-------------------")
+    print(f"Detected Base Release/Test Release information")
+    print(f"-------------------")
+    print(f"Base Release: {base_release_str}")
+    print(f"Path: {base_release['path']}")
+    print()
+    print(f"Test Release: {test_release_str}")
+    print(f"Path: {test_release['path']}")
+    print(f"-------------------")
+
+    # Test for the paths of each of these
+    if not os.path.exists(base_release['path']):
+        print(f"Warning: Base release path does not exist: {base_release['path']}")
+    if not os.path.exists(test_release['path']):
+        print(f"Warning: Test release path does not exist: {test_release['path']}")
+    if not os.path.exists(base_release['path']) or not os.path.exists(test_release['path']):
+        print(f"Fatal Error: One or more release paths do not exist.")
+        # Bail if one of the paths do not exist
+        if not args.override:
+            sys.exit(1)
+
+    # Now check for the install path
+    if not install_paths:
+        print(f"Fatal Error: No install paths found.")
+        if not args.override:
+            sys.exit(1)
+
+    thePath=""
+    theBasePath=""
+    for key, path in install_paths.items():
+        if key == "basepath":
+            print(f"Base Install Path: {path}")
+            thePath=path
+        else:
+            thePath = install_paths["basepath"] + "/" + path
+        if not os.path.exists(thePath):
+            print(f"Warning: Install path does not exist: {thePath}")
+            if args.makedirs:
+                os.makedirs(thePath)
+                print(f"Created missing install directory: {thePath}")
+
+if __name__ == "__main__":
+    main()
